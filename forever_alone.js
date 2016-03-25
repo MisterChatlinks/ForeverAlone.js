d69e7e4fa84abb459a75a36a0597b56109fff26c
Tom Bailey <Tom@Toms-MacBook-Pro.local>
2016-03-25 11:58:14 +0000

    Show loading view while changing view

--- forever_alone.js
+++ forever_alone.js
@@ -1,17 +1,46 @@
 //helpers
 var _fa_http = {
 	"get" : function (url, success, fail) {
+		return this.getWithMinTime(url, success, fail, 0);
+	},
+	"getWithMinTime" : function (url, success, fail, minTime) {
+		console.log("get " + url);
+
+		var start = new Date().getTime();
+
 		var xHttp = new XMLHttpRequest();
 		
+		var that = this;
+
 		xHttp.onreadystatechange = function() {
-			if (xHttp.readyState == 4 && xHttp.status >= 200 && xHttp.status < 300) {
-				success(xHttp.responseText, xHttp.status, xHttp.statusText);
-		    } else {
-		    	fail(xHttp.responseText, xHttp.status, xHttp.statusText);
+			if (xHttp.readyState == 4) {
+				if (xHttp.status >= 200 && xHttp.status < 300) {
+					that.wait(start, minTime, function() {
+						console.log("called back");
+						console.log(xHttp);
+			    		success(xHttp.responseText, xHttp.status, xHttp.statusText);
+			    	});
+				} else {
+					that.wait(start, minTime, function() {
+			    		console.log("called back");
+						console.log(xHttp);
+			    		fail(xHttp.responseText, xHttp.status, xHttp.statusText);
+			    	});
+				}
 		    }
 		}
 		xHttp.open("GET", url, true);
 		xHttp.send();
+	},
+	"wait" : function (start, minTime, callback) {
+		var delta = new Date().getTime() - start;
+		if ((minTime * 1000) > delta) {
+			setTimeout(function() {
+				callback();
+			}, (minTime * 1000) - delta);
+		} else {
+			callback();
+		}
 	}
 };
 
@@ -22,33 +51,39 @@ var _fa_onHashChanged = function() {
 
 		
 		var destination = _fa_findDestinationForHash(hash);
-		//TODO: handle case where no destination found
+		if (destination == null) {
+			_fa_showErrorView();
+		} else {
+			var route = forever_alone.signPost[destination];
 
-		var route = forever_alone.signPost[destination];
+			var matches = hash.match(new RegExp(destination, "i"));
+			for (var index = 1; index < matches.length; index++) {
+				var match = matches[index];
 
-		var matches = hash.match(new RegExp(destination, "i"));
-		for (var index = 1; index < matches.length; index++) {
-			var match = matches[index];
+				route = route.replace("{" + (index-1) + "}", match);
+			}
 
-			route = route.replace("{" + (index-1) + "}", match);
+			_fa_loadView(route);
 		}
-
-		//TODO: show loading view before updating to new view
-		_fa_http.get(route, function(responseText) {
-			forever_alone.viewSection.innerHTML = responseText;
-		}, function(responseText, status) {
-			//TODO: error view
-		});
 	}
 };
 
-var _fa_init = function() {
-	window.onhashchange = _fa_onHashChanged;
+var _fa_loadView = function(toLoad) {
+	forever_alone.viewSection.innerHTML = forever_alone.loadView;
+
+	_fa_http.getWithMinTime(toLoad, function(responseText) {
+		forever_alone.viewSection.innerHTML = responseText;
+	}, function(responseText, status) {
+		_fa_showErrorView();
+	}, 5);
 };
 
-var _fa_findDestinationForHash = function(hash) {
-	console.log(hash);
+var _fa_showErrorView = function() {
+	forever_alone.viewSection.innerHTML = forever_alone.errorView;
+};
 
+
+var _fa_findDestinationForHash = function(hash) {
 	for (var destination in forever_alone.signPost) {
 		var route = forever_alone.signPost[destination];
 
@@ -56,11 +91,23 @@ var _fa_findDestinationForHash = function(hash) {
 			return destination;
 		}
 	}
+
+	console.log("no routes for " + hash);
+	return null;
 };
 
-_fa_init();
 
-//interfaces
+var _fa_init = function() {
+	if (window.location.hash == "") {
+		var endsWithHash = (window.location.toString().substr(window.location.length - 1) == "#");
+		window.location += (endsWithHash ? "" : "#") + "index";
+	}
+
+	window.onhashchange = _fa_onHashChanged;
+	_fa_onHashChanged();
+};
+
+//interface
 var forever_alone = {
 	"viewSection" : null,
 	"signPost" : null,
@@ -87,6 +134,8 @@ var forever_alone = {
 		});
 
 		forever_alone.hashChangedCallback = hashChangedCallback;
+
+		_fa_init();
 	},
 	"onError" : function(errorCallback) {
 		this.errorCallback = errorCallback;
