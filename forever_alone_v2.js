64142882948ada941b431f652cbb8fcffeebdb10
MisterChatlinks <166874035+MisterChatlinks@users.noreply.github.com>
2024-10-19 22:06:09 +0000

    rework: fixed null pointing signPost, converted forever_alone.js to  ECMAScript  class

--- forever_alone_v2.js
+++ forever_alone_v2.js
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
 /**
  * A singleton class that manages routing and view loading based on the URL hash.
  */
