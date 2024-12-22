b89e8c22b93c3e4910c298e9616dec9a6dd543a5
MisterChatlinks <166874035+MisterChatlinks@users.noreply.github.com>
2024-12-22 21:57:46 +0000

    Added syntax enhancement interpreter "ForeverAlone.routeTransformer"

--- dist/foreveralone.js
+++ dist/foreveralone.js
@@ -720,6 +720,7 @@ class _Fo_AppShell {
             if (result) {
                 ForeverAlone.clean(this.#fragment);
                 this.#fragment.appendChild(result);
+                document.title = route.title || _Fo_AppRouter.websiteDefaultTitle;
                 ForeverAlone.emit("app:viewChanged");
                 return true; // return confirmation that everything went well
 
@@ -844,6 +845,11 @@ class _Fo_AppRouter {
     })()
     /** Initializes the router and sets up event listeners for navigation. */
     static init() {
+
+        // save the initial page title
+        this.websiteDefaultTitle = document.title;
+        this.#debug("info", `Initial page title: ${this.websiteDefaultTitle}`);
+
         this.isJQueryAvailable
             ? this.#debug("info", "JQuery detected, igniting router on JQuery Version")
             : this.#debug("info", "No JQuery detected, igniting router on standard Version");
@@ -2546,6 +2552,128 @@ ForeverAlone.addRoutes = (routes) => {
     });
 };  
 
+/**
+ * @purpose :
+ *  Convert enhanced route declaration syntax 
+ *  to the format that can be used by the router.
+ * 
+ * @from 
+ * ForeverAlone.initRoutes({
+ *   "/home": () => renderHome(),
+ * });
+ *
+ *  function renderHome() {
+ *       return {
+ *          title: "ForeverAlone.home",
+ *          content: "/doc/home.html",
+ *           option: {
+ *               method: "post",
+ *               header: { token: "Bearer ..." },
+ *               props: {
+ *                   state: {}
+ *               }
+ *           },
+ *           middleware: [
+ *               () => {
+ *                   if (!isUserLoggedIn) {
+ *                       ForeverAlone.navigate("/login");
+ *                       return false;
+ *                   }
+ *                   return true;
+ *               }
+ *           ],
+ *           children: {
+ *               "/u": () => _renderU(),
+ *               "/:articleId": () => _renderArticleWithID()
+ *           }
+ *       };
+ *   }
+ *
+ *   function _renderU() {
+ *       return {
+ *           title: "ForeverAlone.u",
+ *           content: "/doc/u.html",
+ *           option: {
+ *               method: "get",
+ *               header: { token: "Bearer ..." },
+ *               props: {
+ *                   state: {}
+ *               }
+ *           }
+ *       };
+ *  }
+ *
+ *   function _renderArticleWithID() {
+ *       return {
+ *           title: "ForeverAlone.article",
+ *           option: {
+ *               props: {
+ *                   state: {}
+ *               }
+ *           },
+ *           children: {
+ *               "/:deprecated": () => _renderIsDeprecatedArticle()
+ *           }
+ *       };
+ *   }
+ *
+ *   function _renderIsDeprecatedArticle() {
+ *       return {
+ *           title: "ForeverAlone.isDeprecated",
+ *           path: "/doc/whoops.html",
+ *           option: {
+ *               method: "get",
+ *               header: { token: "Bearer ..." },
+ *               props: {
+ *                   state: {}
+ *               }
+ *           }
+ *       };
+ *   }
+ *
+ *  @to 
+ *
+ *   let routes = [
+ *       { key: "/home", path: "home-path", ...option, children:[
+ *           { key: "/u", path: "u-path", ...option, children: [] },
+ *           { key: `/:article type:${type}`, path: "home-path", ...option, children: [
+ *               { key: "deprecated", path: "deprecated-path", ...option, children: [] }
+ *           ] }
+ *       ]}
+ *  ]
+ * 
+ *  ForeverAlone.addRoutes (routes)
+ * */
+ForeverAlone.routeTransformer = (routes) => {
+
+    let result = [];
+
+    let nodes = Object.keys(routes);
+    nodes
+        .forEach(node => {
+            if (typeof routes[node] == "function") {
+                routes[node] = routes[node]();
+            }
+            routes[node].key = routes[node].type ? `${node} type:${routes[node].type}` : node;
+            routes[node] = {...routes[node], ...( routes[node].option || {} )  }
+
+            if(routes[node].content){
+                routes[node].path = routes[node].content;
+            } 
+
+            delete routes[node].type;
+            delete routes[node].option;
+            delete routes[node].content;
+
+            if(routes[node].children){
+                routes[node].children = parseRoutes(routes[node].children)
+            }
+            result.push(routes[node]);
+        }
+    )
+
+    return result;
+}
 
 _Fa_DebugHelper.toggleDebugAllTrue()
 
